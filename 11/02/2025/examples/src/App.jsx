1fa6e39576025a2d44584b9e0266554365cc02aa
Aishu ks <aishuks2005@gmail.com>
2025-02-11 12:59:50 +0530

    day 2-react

---
+++ 11/02/2025/examples/src/App.jsx
@@ -0,0 +1,16 @@
+import Greetings from "./greetings";
+
+export default function App(){
+return(
+  <>
+    <h1><marquee direction="left" scrollamount="20">Welcome to REACT</marquee>
+  
+  </h1>
+  <Greetings/>
+  </>
+);
+}
+
+
+
+
---
+++ 11/02/2025/examples/src/Greetings.jsx
@@ -0,0 +1,8 @@
+function Greetings()
+{
+    return(
+<>
+    <h3>Hi Im greetings child component!</h3></>
+    );
+}
+export default Greetings;
